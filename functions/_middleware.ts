1d3904cd4a4b72f66e3dbe45877cc98f52a7a82e
Kerry Hatcher <kerry.hatcher@protonmail.com>
2023-01-22 22:58:28 -0500

    Add password

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,8 +1,47 @@
 import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
+import { CFP_ALLOWED_PATHS } from './constants';
+import { getCookieKeyValue } from './utils';
+import { getTemplate } from './template';
 
-export const onRequest: PagesFunction[] = [
+async function password_site(context: {
+  request: Request;
+  next: () => Promise<Response>;
+  env: { CFP_PASSWORD?: string };
+}): Promise<Response> {
+  const { request, next, env } = context;
+  const { pathname, searchParams } = new URL(request.url);
+  const { error } = Object.fromEntries(searchParams);
+  const cookie = request.headers.get('cookie') || '';
+  const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
+
+  if (
+    cookie.includes(cookieKeyValue) ||
+    CFP_ALLOWED_PATHS.includes(pathname) ||
+    !env.CFP_PASSWORD
+  ) {
+    // Correct hash in cookie, allowed path, or no password set.
+    // Continue to next middleware.
+    return await next();
+  } else {
+    // No cookie or incorrect hash in cookie. Redirect to login.
+    return new Response(getTemplate({ redirectPath: pathname, withError: error === '1' }), {
+      headers: {
+        'content-type': 'text/html'
+      }
+    });
+  }
+}
+
+const PagesFunction = [
+
+]
+
+export const onRequest = [
+
+  password_site,
+
   staticFormsPlugin({
     respondWith: ({ formData, name }) => {
           const email = formData.get('email')
@@ -19,5 +58,4 @@ export const onRequest: PagesFunction[] = [
       return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
     },
 })
-]
-
+];
