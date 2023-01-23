c732d6973ac0f9c18d3908b8e7be99c783056575
Kerry Hatcher <kerry.hatcher@protonmail.com>
2023-01-22 22:27:53 -0500

    Add email to form

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,4 +1,5 @@
 import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
 
 export const onRequest: PagesFunction[] = [
@@ -7,5 +8,16 @@ export const onRequest: PagesFunction[] = [
           const email = formData.get('email')
           return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)   
     }
-  })
+  }),
+  mailChannelsPlugin({
+    personalizations: [{
+      to: [{ name: "Kerry Hatcher", email: "kerry.hatcher@gasdf.us" }],
+    },],
+    from: { name: "Website Form", email: "noreply@gasdf.us", },
+    
+    respondWith: () => {
+      return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
+    },
+})
 ]
+
