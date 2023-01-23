d91c7414a3cbff517e948d0eecb8e8b0c11f7b6a
Kerry Hatcher <kerry.hatcher@protonmail.com>
2023-01-22 22:20:52 -0500

    Fix Middleware

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,17 +1,11 @@
 import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
 
 
-export const onRequest: PagesFunction = staticFormsPlugin({
-  respondWith: ({ formData, name }) => {
-        const email = formData.get('email')
-        console.log(Response)
-        var response = new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`);
-        
-        var resp = response.redirect("/", 302)
-        return resp   
-  }
-});
-
-
-
-export const onRequest = [errorHandling, authentication];
+export const onRequest: PagesFunction[] = [
+  staticFormsPlugin({
+    respondWith: ({ formData, name }) => {
+          const email = formData.get('email')
+          return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)   
+    }
+  })
+]
