02a2104789ec322a0b58ea0209eed6ea69f0f545
Kerry Hatcher <kerry.hatcher@protonmail.com>
2023-01-31 02:58:58 -0500

    ts working

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,19 +1,17 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = [
-  mailChannelsPlugin({
-    personalizations: [
-      {
-        to: [{ name: "Kerry Hatcher", email: "kerry.hatcher@gasdf.us" }],
-      },
-    ],
-    from: { name: "Website Form", email: "noreply@gasdf.us" },
-    respondWith: () => {
-      // return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
-      return new Response(null, {
-        status: 302,
-        headers: { Location: "/pages/join_form_message/" },
-      });
+export const onRequest: PagesFunction = mailChannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "Kerry Hatcher", email: "kerry.hatcher@gasdf.us" }],
     },
-  }),
-];
+  ],
+  from: { name: "Website Form", email: "noreply@gasdf.us" },
+  respondWith: () => {
+    // return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
+    return new Response(null, {
+      status: 302,
+      headers: { Location: "/pages/join_form_message/" },
+    });
+  },
+});
