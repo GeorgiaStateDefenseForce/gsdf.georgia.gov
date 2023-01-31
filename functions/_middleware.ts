2a920456297ea29770b811266aa3f2940670dc1d
Kerry Hatcher <kerry.hatcher@protonmail.com>
2023-01-31 02:52:31 -0500

    fix form maybe?

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,15 +1,19 @@
 import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({
-  personalizations: [
-    {
-      to: [{ name: "Kerry Hatcher", email: "kerry.hatcher@gasdf.us" }],
+export const onRequest = [
+  mailChannelsPlugin({
+    personalizations: [
+      {
+        to: [{ name: "Kerry Hatcher", email: "kerry.hatcher@gasdf.us" }],
+      },
+    ],
+    from: { name: "Website Form", email: "noreply@gasdf.us" },
+    respondWith: () => {
+      // return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
+      return new Response(null, {
+        status: 302,
+        headers: { Location: "/pages/join_form_message/" },
+      });
     },
-  ],
-  from: { name: "Website Form", email: "noreply@gasdf.us" },
-  respondWith: () => {
-    return new Response(
-      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-    );
-  },
-});
+  }),
+];
