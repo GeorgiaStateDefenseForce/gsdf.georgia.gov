48e4ace1147b250ffbe1a5bf2b541aa7e2620b5f
Ryan Taylor <6969996+rtaylor82@users.noreply.github.com>
2023-07-31 18:15:59 +0000

    add stars to req flds; clean up comments; change response code;

--- functions/api/cform.js
+++ functions/api/cform.js
@@ -1,14 +1,6 @@
-// import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
-
-// export const onRequest: PagesFunction = staticFormsPlugin({
-//   respondWith: ({ formData, name }) => {
-//     const email = formData.get('email')
-//     return new Response(`Hello, ${email}! Thank you for submitting the ${name} form.`)
-//   }
-// });
 
 export async function onRequestPost(context) {
-  // try {
+  
     let input = await context.request.formData();
     // Convert FormData to JSON
     // NOTE: Allows multiple values per key
@@ -23,9 +15,7 @@ export async function onRequestPost(context) {
       }
     }
     console.log(input);
-  // } catch (err) {
-    // return new Response('Error parsing JSON content', { status: 400 });
-  // }
+
 
   let token, secret;
   token = input.get('cf-turnstile-response') ? input.get('cf-turnstile-response').toString() : false;
@@ -59,16 +49,6 @@ export async function onRequestPost(context) {
   //}
   input.delete("cf-turnstile-response");
 
-  // let googleFormData = new FormData();
-  // googleFormData.append('entry.299316044', input.get("fname").toString());
-  // googleFormData.append('entry.246872012', input.get("lname").toString());
-  // googleFormData.append('entry.1819157110', input.get("email").toString());
-  // googleFormData.append('entry.1829779236', input.get("phone").toString());
-  // googleFormData.append('entry.1152223255', input.get("city").toString());
-  // googleFormData.append('entry.433270788', input.get("state").toString());
-  // googleFormData.append('entry.1514106883', input.get("findout").toString());
-  // googleFormData.append('entry.909904957', input.get("preferred").toString());
-  // console.log(googleFormData);
   let googleUrl = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeJAeEHO1H4vpLkMdwG1kc_U4KtAEAYFqbgeDXbhDpXQuFpvA/formResponse';
   // let googleResult = await 
   fetch(googleUrl, {
@@ -92,19 +72,9 @@ export async function onRequestPost(context) {
   .catch(error => {
     console.log("Error: " + error);
   });;
-  // let googleOutcome = await googleResult.json();
-  // console.log(googleOutcome);
-
-
-
+  
   const destinationURL = "https://gsdf.georgia.gov/thank-you/";
-  const statusCode = 200;
+  const statusCode = 303;
   return Response.redirect(destinationURL, statusCode);
-  // return new Response('done');
-  // let pretty = JSON.stringify(output, null, 2);
-  // return new Response(pretty, {
-  //   headers: {
-  //     'Content-Type': 'application/json;charset=utf-8',
-  //   },
-  // });
+  
 }
